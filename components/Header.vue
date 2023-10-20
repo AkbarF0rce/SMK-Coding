f5911734a24e7698ab291b7d704a4f2977a84ada
AkbarF0rce <akbarridhoarrafi@gmail.com>
2023-10-20 15:32:41 +0700

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+
+import { NuxtLink } from '#build/components';
+<template>
+    <header class="w-full border-b border-slate-200 py-6">
+        <div class="container">
+            <div class="flex justify-between items-center">
+                <div>
+                    <NuxtLink to="/" class="text-xl font-bold">RxZyy Shop</NuxtLink>
+                </div>
+                <nav class="flex items-center gap-6">
+                    <NuxtLink to="/" class="text-base">Home</NuxtLink>
+                    <NuxtLink to="/product" class="text-base">Products</NuxtLink>
+                    <NuxtLink to="/cart" class="text-base">Cart</NuxtLink>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
