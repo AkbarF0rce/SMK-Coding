3ecaa371414a1e650f91602b2c89960c3a7cd317
AkbarF0rce <akbarridhoarrafi@gmail.com>
2023-10-21 07:31:21 +0700

    Update 21-10-2023

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -14,10 +14,10 @@
     <div v-show="isShow" class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-xl w-full absolute top-14">
         <ul class="py-2 text-sm text-gray-700">
             <li>
-                <span class="block px-4 py-2 hover:bg-primary hover:text-white transition duration-200 cursor-pointer" @click="$emit('selectedCategory', '')">All</span>
+                <span class="block px-4 py-2 hover:bg-primary hover:text-white transition duration-200 cursor-pointer" @click="isShow = !isShow, $emit('selectedCategory', '')">All</span>
             </li>
             <li v-for="(item, index) in category" :key="index">
-                <span class="block px-4 py-2 hover:bg-primary hover:text-white transition duration-200 cursor-pointer" @click="$emit('selectedCategory', item.name)">{{ item?.name }}</span>
+                <span class="block px-4 py-2 hover:bg-primary hover:text-white transition duration-200 cursor-pointer" @click="isShow = !isShow, $emit('selectedCategory', item.name)">{{ item?.name }}</span>
             </li>
         </ul>
     </div>
